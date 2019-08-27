551f33dc5b901e322b26d384a52563d1d84d1c5b
1361356742@qq.vom <1361356742@qq.com>
2019-08-27 09:06:07 +0800

    修改bug;

--- app/src/main/java/com/zfg/test/activity/MyEditTextActivity.java
+++ app/src/main/java/com/zfg/test/activity/MyEditTextActivity.java
@@ -6,18 +6,36 @@ import android.view.View;
 import android.widget.Button;
 
 import com.zfg.test.R;
+import com.zfg.test.activity.base.BaseActivity;
 
 import io.reactivex.Observable;
 
 
-public class MyEditTextActivity extends AppCompatActivity {
+public class MyEditTextActivity extends BaseActivity {
     Button mButton;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_my_edit_text);
-        mButton = findViewById(R.id.click_btn);
+    protected int getLayoutId() {
+        return R.layout.activity_my_edit_text;
+    }
 
+    @Override
+    protected void setupView() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected void addListener() {
+
+    }
+
+    @Override
+    protected int getTitleStringId() {
+        return 0;
     }
 }
