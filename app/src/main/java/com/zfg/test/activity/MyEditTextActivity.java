d2f76109f49ae1f0b950de801491dcca9161baf5
1361356742@qq.vom <1361356742@qq.com>
2019-08-28 15:39:38 +0800

    优化代码；

--- app/src/main/java/com/zfg/test/activity/MyEditTextActivity.java
+++ app/src/main/java/com/zfg/test/activity/MyEditTextActivity.java
@@ -1,14 +1,9 @@
 package com.zfg.test.activity;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 
 import com.zfg.test.R;
 import com.zfg.test.activity.base.BaseActivity;
-
-import io.reactivex.Observable;
 
 
 public class MyEditTextActivity extends BaseActivity {
