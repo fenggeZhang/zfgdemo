73ac2b1257624ca71a421204d6e359da00e67c2f
1361356742@qq.vom <1361356742@qq.com>
2019-09-19 11:20:30 +0800

    格式化；

--- app/src/main/java/com/zfg/test/algorithm/AddSumTest.java
+++ app/src/main/java/com/zfg/test/algorithm/AddSumTest.java
@@ -12,21 +12,21 @@ import java.util.Scanner;
  */
 public class AddSumTest {
 
-    public static void main(String args[]){
+    public static void main(String args[]) {
         Scanner in = new Scanner(System.in);
         int num = in.nextInt();
-        Map<Integer,Integer>  integerMap=new HashMap<Integer, Integer>() ;
+        Map<Integer, Integer> integerMap = new HashMap<Integer, Integer>();
         for (int i = 0; i < num; i++) {
-            int aNum=in.nextInt();
+            int aNum = in.nextInt();
             for (int j = 0; j < aNum; j++) {
-                int value=0;
-                if(integerMap.get(i)!=null){
-                    value=integerMap.get(i);
+                int value = 0;
+                if (integerMap.get(i) != null) {
+                    value = integerMap.get(i);
                 }
-                integerMap.put(i,value+in.nextInt());
+                integerMap.put(i, value + in.nextInt());
             }
         }
-        for (int i = 0; i <num ; i++) {
+        for (int i = 0; i < num; i++) {
             System.out.println(integerMap.get(i));
         }
     }
