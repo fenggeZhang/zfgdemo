379bf6a38ca9f872ba9a9126fb2f1b5b32b50f05
1361356742@qq.vom <1361356742@qq.com>
2019-12-05 09:31:26 +0800

    测试；

---
+++ app/src/main/java/com/zfg/test/javatest/StringTest.java
@@ -0,0 +1,13 @@
+package com.zfg.test.javatest;
+
+/**
+ * author : zfg
+ * e-mail : zfg_android@163.com
+ * date   : 2019/11/22
+ * desc   :
+ */
+public class StringTest {
+    public static void main(String[] args) {
+
+    }
+}
